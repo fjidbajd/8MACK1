3091173b5d5947bd65b7013fe0e72f1579454b72
fjidbajd <128946062+fjidbajd@users.noreply.github.com>
2024-08-04 08:08:02 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9450,13 +9450,8 @@ menu() {
     echoContent green "作者：mack-a"
     echoContent green "当前版本：v3.3.12"
     echoContent green "Github：https://github.com/mack-a/v2ray-agent"
-    echoContent green "描述：八合一共存脚本\c"
     showInstallStatus
     checkWgetShowProgress
-    echoContent red "\n=========================== 推广区============================"
-    echoContent red "                                              "
-    echoContent green "VPS选购攻略：https://www.v2ray-agent.com/archives/1679975663984"
-    echoContent green "年付10美金低价VPS AS4837：https://www.v2ray-agent.com/archives/racknerdtao-can-zheng-li-nian-fu-10mei-yuan"
     echoContent red "=============================================================="
     if [[ -n "${coreInstallType}" ]]; then
         echoContent yellow "1.重新安装"
